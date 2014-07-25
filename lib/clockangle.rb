60fe514b72e67028e834187cec5fd60dffe6c0c4
Anna Valkov & Nuatu Tseggai <student@epicodus.com>
2014-07-25 11:07:56 -0700

    evaluates the distance between the two hands and returns the smallest one

--- lib/clockangle.rb
+++ lib/clockangle.rb
@@ -1,4 +1,16 @@
 def clock_angle(hr,min)
- "0 degrees"
+
+  hr == 0 if hr == 12
+
+  hr_angle = (hr*30)+(min*0.5)
+  min_angle = min*6
+  angle1 = ((360 - hr_angle) + min_angle).abs
+  angle2 = (hr_angle - min_angle).abs
+
+  angle1 > angle2 ? result = angle2 : result = angle1
+ #this 'Ternary Operator' assigns the lowest angle to the variable "result"
+
+  p result.to_s + " degrees"
 end
 
+clock_angle(12,00)
--- spec/clockangle_spec.rb
+++ spec/clockangle_spec.rb
@@ -3,6 +3,10 @@ require('clockangle')
 
 describe('clock_angle') do
   it("returns 0 degrees if the hour and minute hand are at the same place") do
-    clock_angle(12,00).should(eq("0 degrees"))
+    expect(clock_angle(12,00)).to(eq("0.0 degrees"))
+  end
+
+  it("returns the smaller angle depending on the position of the two hands") do
+    expect(clock_angle(12,15)).to(eq("82.5 degrees"))
   end
 end
